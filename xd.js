6588e1791860c2b7a8ede0eeecad30c5aee984a4
sankiago <69083739+sankiago@users.noreply.github.com>
2021-03-08 11:30:53 -0500

    Update xd.js
    efectos del hover a los students

--- xd.js
+++ xd.js
@@ -1,1 +1,97 @@
-console.log('funcionando')
+console.log('cargando página...')
+
+//hacemos que el Script se ejecute cuando la página cargue completamente
+window.onload = script
+
+//Código
+function script ()
+{
+    //Página cargada
+    console.log('cargada :D')
+    
+    //Trayendo las listas principales del DOM
+    const listasNodeList = document.querySelectorAll('.lst__item.lvl1')
+    const listasArray = [...listasNodeList]
+    console.log(listasArray)
+    listasArray.forEach((lista) => {
+        const titulo = lista.querySelector('.lst__title.lvl1')
+        titulo.addEventListener('mouseup', manejador)
+    });
+    
+    //funcion que maneja las listas
+    function manejador()
+    {
+        console.log('oprimidoxd')
+        listasArray.forEach((lista) => {
+/*         const titulo = lista.querySelector('.lst__title.lvl1')
+ */        lista.open = false 
+        })
+
+        /* console.log(this.parentElement.open)
+
+        if (this.parentElement.open)
+        {
+            this.parentElement.classList.remove('open')
+        }
+        else {
+            this.classList.parentElement.classList.add('open')
+        } */
+    }
+
+
+    //manejando el boton cerrar todo
+    const btn = document.querySelector('.btn')
+    btn.addEventListener('mouseup', manejador)
+
+
+    //Efectos para los estudiantes
+        //Traer students elements del DOM
+        const stuWrp_nodeList = document.querySelectorAll('.studentWrapper')
+        const stuWrp_array = [...stuWrp_nodeList]
+        
+        stuWrp_array.forEach((item) => {
+            item.addEventListener('mouseover', showStudentDet)
+            item.addEventListener('mouseleave', hideStudentDet)
+        })
+
+        function showStudentDet ()
+        {
+            const nombre = this.querySelector('.student__name')
+            const description = this.querySelector('.student__description')
+            const letra = this.querySelector('.student__letter')
+            
+            this.classList.add('bg_black')
+
+            console.log(`hover on ${nombre.textContent}`)
+
+            nombre.classList.remove('hidden')
+            description.classList.remove('hidden')
+        }
+
+        function hideStudentDet ()
+        {
+            const nombre = this.querySelector('.student__name')
+            const description = this.querySelector('.student__description')
+            const letra = this.querySelector('.student__letter')
+
+            this.classList.remove('bg_black')
+
+            console.log(`leaving ${nombre.textContent}`)
+
+            nombre.classList.add('hidden')
+            description.classList.add('hidden')
+        }
+
+        
+       /*  const stuName_nodeList = document.querySelector('.student__name')
+        const stuName_array = [...stuName_nodeList]
+        
+        const stuDts_nodeList = document.querySelector('.student__description')
+        const stuDts_array = [...stuDts_nodeList]
+        
+        const stuLetter_nodeList = document.querySelector('.student__letter')
+        const stuLetter_array = [...stuLetter_nodeList] */
+
+
+        
+}   
